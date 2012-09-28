3d4157e8a5c50da220514bc8bda3e7bb31b111d0
Steve Clement <steve@localhost.lu>
2012-09-28 10:38:13 +0200

    reenabled meta-refresh, added web update script

--- scripts/printus.sh
+++ scripts/printus.sh
@@ -4,10 +4,10 @@ while [ true ]; do
 
 if [ -e print.txt ]; then
   # Use this line for printing to a Named Printer
-  #lpr -PDYMO_LabelWriter_450 -o landscape -o Pagesize=w68h252 print.txt && rm print.txt
+  lpr -PDYMO_LabelWriter_450 -o landscape -o Pagesize=w68h252 print.txt && rm print.txt
   # Use this line for printing to the default printer (if defined)
-  lpr -o landscape -o Pagesize=w68h252 print.txt && rm print.txt
+  #lpr -o landscape -o Pagesize=w68h252 print.txt && rm print.txt
 fi
 
-sleep 1
+sleep 4
 done
---
+++ scripts/update_web.sh
@@ -0,0 +1,8 @@
+while [ true ]; do
+
+./fb-web.py
+
+echo "Sleeping for 10 seconds..."
+
+sleep 10
+done
